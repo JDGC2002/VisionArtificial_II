c57344ef2778803130dc7547673d50225188c8db
Jhon Gomez <jhondy141516@hotmail.com>
2023-02-04 20:40:52 -0500

    Ejercicios imagenes transformaciones

---
+++ Transformaciones/ejercicio2.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+
+imgF = cv.imread('test_im.jpg')
+imga = cv.imread('test_im-A.jpg')
+imgB = cv.imread('test_im-B.jpg')
+
+def show_im(img):
+    cv.imshow('Window', img)
+    cv.waitKey(0)
+
+def imageMatting():
+    return (1-imga)*imgB + imga*imgF
+
+imgC = imageMatting()
+show_im(imgC)
---
+++ Transformaciones/ejercicio3.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+# Load the image
+img = cv2.imread("cat.jpg")
+
+# Seleccionar el canal verde
+canal_verde = img[:,:,1]
+
+# Aplicar la condición y asignar 0 a los valores mayores a 200
+canal_verde = np.where(canal_verde > 100, 0, canal_verde)
+
+# Reemplazar el canal verde en la img original
+img[:,:,1] = canal_verde
+
+# Crear una máscara booleana a partir de la condición anterior
+mascara = canal_verde == 0
+
+# Aplicar la máscara al canal azul y asignar 255 a los valores correspondientes
+np.copyto(img[:,:,2], 255, where=mascara)
+
+# Aplicar la máscara al canal rojo y asignar 0 a los valores correspondientes
+np.copyto(img[:,:,0], 0, where=mascara)
+
+def show_im(img):
+    cv2.imshow('Window', img)
+    cv2.waitKey(0)
+    
+show_im(img)
