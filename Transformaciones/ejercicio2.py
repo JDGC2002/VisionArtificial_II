71036e546881b7d196bbbb3b3258b7e93e596470
Jhon Gomez <jhondy141516@hotmail.com>
2023-02-08 19:24:27 -0500

    ejercicio operadores punto

--- Transformaciones/ejercicio2.py
+++ Transformaciones/ejercicio2.py
@@ -1,20 +1,33 @@
 import cv2 as cv
 import numpy as np
 
-imgF = cv.imread('test_im.jpg')
-imga = cv.imread('test_im-A.jpg')
-imgB = cv.imread('test_im-B.jpg')
-
-mascara1 = imga[:,:,0] <= 20
-mascara2 = imga[:,:,1] <= 20
-mascara3 = imga[:,:,2] <= 20
-
-np.copyto(imgF[:,:,0], imgB[:,:,0], where=mascara1)
-np.copyto(imgF[:,:,1], imgB[:,:,1], where=mascara2)
-np.copyto(imgF[:,:,2], imgB[:,:,2], where=mascara3)
+imgcat = cv.imread('test_im.jpg')
+dia = cv.imread('day.jpg')
+noche = cv.imread('night.jpg')
 
 def show_im(img):
     cv.imshow('Window', img)
     cv.waitKey(0)
+    
+def brillo_contraste(img, gain, bias):
+    return (img * gain) + bias
 
-show_im(imgF)
+def mezcla_lineal(img1, img2, alpha):
+    img1 = np.array(img1)
+    img2 = np.array(img2)
+    return ((1-alpha)*img1) + (img2*alpha)
+
+def correccion_gamma(image, gamma=1.0):
+    gamma_corr = image / 255.0
+    gamma_corr = np.power(gamma_corr, gamma)
+    gamma_corr = np.uint8(gamma_corr * 255)
+
+    return gamma_corr
+
+
+brillo_contras = brillo_contraste(imgcat, 1.5, 0.2)
+mezcla_lin = mezcla_lineal(dia, noche, 0)
+correcion_gam = correccion_gamma(imgcat, 2)
+show_im(brillo_contras)
+show_im(mezcla_lin)
+show_im(correcion_gam)
